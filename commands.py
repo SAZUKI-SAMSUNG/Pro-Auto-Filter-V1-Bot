a072858712c9b980aa11d0bd5ad6af20c04f9254
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-01-29 15:39:52 +0530

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @TEAMSAZUKI
+
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from script import script
+
+
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(client, message):
+    try:
+        await message.reply_text(
+            text=script.START_MSG.format(message.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("HELP", callback_data="help_data"),
+                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "𝙹𝙾𝙸𝙽 𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url="https://t.me/+n33_2k2_9vZlMWY1")
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
+    except:
+        pass
+
+@Client.on_message(filters.command(["help"]) & filters.private)
+async def help(client, message):
+    try:
+        await message.reply_text(
+            text=script.HELP_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("BACK", callback_data="start_data"),
+                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "𝚂𝚄𝙿𝙿𝙾𝚁𝚃", url="https://t.me/oesuppert")
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
+    except:
+        pass
+
+@Client.on_message(filters.command(["about"]) & filters.private)
+async def about(client, message):
+    try:
+        await message.reply_text(
+            text=script.ABOUT_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("BACK", callback_data="help_data"),
+                        InlineKeyboardButton("START", callback_data="start_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "SOURCE CODE", url="https://github.com/MRK-YT/Auto-Filter-Bot")
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
+    except:
+        pass
