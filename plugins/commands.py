3fa54c36a158ac9294b959cc443c221db09636e1
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-01-29 15:51:08 +0530

    Create commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/MRK-YT/Auto-Filter-Bot")
+                            "SOURCE CODE", url="https://t.me/NOKKIERUNNOEPPPKITTUM")
                     ]
                 ]
             ),
