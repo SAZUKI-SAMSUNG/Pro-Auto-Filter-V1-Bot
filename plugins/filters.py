cd5774e13e4f6e022d0d383e11e188840385ffdb
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-01-29 15:51:38 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -1,1 +1,208 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @TEAMSAZUKI
 
+
+import re
+import pyrogram
+
+from pyrogram import (
+    filters,
+    Client
+)
+
+from pyrogram.types import (
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup, 
+    Message,
+    CallbackQuery
+)
+
+from bot import Bot
+from script import script
+from config import MAINCHANNEL_ID
+
+BUTTONS = {}
+ 
+@Client.on_message(filters.group & filters.text)
+async def filter(client: Bot, message: Message):
+    if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
+        return
+
+    if len(message.text) > 2:    
+        btn = []
+        async for msg in client.USER.search_messages(MAINCHANNEL_ID,query=message.text,filter='document'):
+            file_name = msg.document.file_name
+            msg_id = msg.message_id                     
+            link = msg.link
+            btn.append(
+                [InlineKeyboardButton(text=f"{file_name}",url=f"{link}")]
+            )
+
+        if not btn:
+            return
+
+        if len(btn) > 10: 
+            btns = list(split_list(btn, 10)) 
+            keyword = f"{message.chat.id}-{message.message_id}"
+            BUTTONS[keyword] = {
+                "total" : len(btns),
+                "buttons" : btns
+            }
+        else:
+            buttons = btn
+            buttons.append(
+                [InlineKeyboardButton(text="𝙿𝙰𝙶𝙴𝚂 1/1",callback_data="pages")]
+            )
+            await message.reply_text(
+                f"<b> Here is the result for {message.text}</b>",
+                reply_markup=InlineKeyboardMarkup(buttons)
+            )
+            return
+
+        data = BUTTONS[keyword]
+        buttons = data['buttons'][0].copy()
+
+        buttons.append(
+            [InlineKeyboardButton(text="𝙽𝙴𝚇𝚃",callback_data=f"next_0_{keyword}")]
+        )    
+        buttons.append(
+            [InlineKeyboardButton(text=f"𝙿𝙰𝙶𝙴𝚂 1/{data['total']}",callback_data="pages")]
+        )
+
+        await message.reply_text(
+                f"<b> Here is the result for {message.text}</b>",
+                reply_markup=InlineKeyboardMarkup(buttons)
+            )    
+
+
+@Client.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    if query.message.reply_to_message.from_user.id == query.from_user.id:
+
+        if query.data.startswith("next"):
+            await query.answer()
+            ident, index, keyword = query.data.split("_")
+            data = BUTTONS[keyword]
+
+            if int(index) == int(data["total"]) - 2:
+                buttons = data['buttons'][int(index)+1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton("𝙱𝙰𝙲𝙺", callback_data=f"back_{int(index)+1}_{keyword}")]
+                )
+                buttons.append(
+                    [InlineKeyboardButton(f"𝙿𝙰𝙶𝙴𝚂{int(index)+2}/{data['total']}", callback_data="pages")]
+                )
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return
+            else:
+                buttons = data['buttons'][int(index)+1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton("𝙱𝙰𝙲𝙺", callback_data=f"back_{int(index)+1}_{keyword}"),InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{int(index)+1}_{keyword}")]
+                )
+                buttons.append(
+                    [InlineKeyboardButton(f"𝙿𝙰𝙶𝙴𝚂 {int(index)+2}/{data['total']}", callback_data="pages")]
+                )
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return
+
+
+        elif query.data.startswith("back"):
+            await query.answer()
+            ident, index, keyword = query.data.split("_")
+            data = BUTTONS[keyword] 
+
+            if int(index) == 1:
+                buttons = data['buttons'][int(index)-1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton("𝙽𝙴𝚇𝚃", callback_data=f"next_{int(index)-1}_{keyword}")]
+                )
+                buttons.append(
+                    [InlineKeyboardButton(f"𝙿𝙰𝙶𝙴𝚂 {int(index)}/{data['total']}", callback_data="pages")]
+                )
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return   
+            else:
+                buttons = data['buttons'][int(index)-1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton("𝙱𝙰𝙲𝙺", callback_data=f"back_{int(index)-1}_{keyword}"),InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{int(index)-1}_{keyword}")]
+                )
+                buttons.append(
+                    [InlineKeyboardButton(f"𝙿𝙰𝙶𝙴𝚂 {int(index)}/{data['total']}", callback_data="pages")]
+                )
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return
+
+
+        elif query.data == "pages":
+            await query.answer()
+
+
+        elif query.data == "start_data":
+            await query.answer()
+            keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton("HELP", callback_data="help_data"),
+                    InlineKeyboardButton("ABOUT", callback_data="about_data")],
+                [InlineKeyboardButton("𝙹𝙾𝙸𝙽 𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url="https://t.me/+n33_2k2_9vZlMWY1")]
+            ])
+
+            await query.message.edit_text(
+                script.START_MSG.format(query.from_user.mention),
+                reply_markup=keyboard,
+                disable_web_page_preview=True
+            )
+
+
+        elif query.data == "help_data":
+            await query.answer()
+            keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton("BACK", callback_data="start_data"),
+                    InlineKeyboardButton("ABOUT", callback_data="about_data")],
+                [InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃", url="https://t.me/oesuppert")]
+            ])
+
+            await query.message.edit_text(
+                script.HELP_MSG,
+                reply_markup=keyboard,
+                disable_web_page_preview=True
+            )
+
+
+        elif query.data == "about_data":
+            await query.answer()
+            keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton("BACK", callback_data="help_data"),
+                    InlineKeyboardButton("START", callback_data="start_data")],
+                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/MRK-YT/Auto-Filter-Bot")]
+            ])
+
+            await query.message.edit_text(
+                script.ABOUT_MSG,
+                reply_markup=keyboard,
+                disable_web_page_preview=True
+            )
+
+
+    else:
+        await query.answer("Thats not for you!!",show_alert=True)
+
+
+def split_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
